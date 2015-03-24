d5be9dfb59b9ecef5bc90a18be274a07ea79b234
nightsign <zhangsunan1982@gmail.com>
2015-03-24 17:59:21 +0800

    push first step

---
+++ bookmark_fns.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunan
+ * Date: 2015/3/24
+ * Time: 17:34
+ */
+//require_once('data_valid_fns.php');
+//require_once('db_fns.php');
+//require_once('user_auth_fns.php');
+require_once('output_fns.php');
+//require_once('url_fns.php');
---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunan
+ * Date: 2015/3/24
+ * Time: 17:33
+ */
+    include_once('bookmark_fns.php');
+        do_html_header('');
+//    display_sitinfo();
+//    display_login_form();
+//    do_html_footer();
+?>
